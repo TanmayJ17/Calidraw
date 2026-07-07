93aac522ab7ace6ad614de925b230372656cceba
Tanmay Jain <tanmayjain@Tanmays-MacBook-Air.local>
2026-07-07 15:35:51 +0530

    calidraw

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://calidraw-frontend.onrender.com';
+const BASE_URL = 'https://calidraw.onrender.com';
 
 export async function apiFetch(path, options = {}) {
   const token = localStorage.getItem('token');
